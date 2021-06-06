8b62d6e7c1c31dc8d73e70f472a16d3575f2ad8a
Daniel F. Alvarez Caballero <ingdanielf8@gmail.com>
2021-06-06 01:07:06 -0500

    Avance 3 CF2-Mercadeo Falta actualización del diseño

---
+++ frontend-sn/desarrollo/ECORED-BASE-2021-master-v3.2.0/src/views/Tema4.vue
@@ -0,0 +1,113 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    .titulo-principal
+      .titulo-principal__numero
+        span 4
+      h1 Preproducción
+    p.mb-4 Todo proyecto nace con la necesidad de resolver, generar o ejecutar una idea en particular. Este momento de creación es llamado preproducción y comprende en este caso la creación de diferentes componentes como el guion y el storyboard. Esta fase permite reconocer cuáles herramientas y recursos serán necesarios, para el desarrollo del proyecto en general.
+    p.mb-4 El siguiente cuadro presenta los pasos a seguir en preproducción, de un proyecto gráfico, desde el inicio del proyecto hasta las decisiones y acciones tomadas previas a su ejecución. 
+    figure.mb-3
+      img(src="@/assets/template/tema-4-1.png", alt="Texto que describa la imagen")
+    figcaption.mb-5 Referencia  Adaptado de Sepulveda y Quiceno (2015)
+    LineaTiempoD.color-acento-contenido.mb-5
+      .row(numero="1" titulo="Necesidad: ")
+        .col-md-4
+          figure
+            img(src='@/assets/template/tema-4-2.png', alt='Texto que describa la imagen')
+
+        .col-md-8.mb-4.mb-md-0
+          p La necesidad se encuentra enmarcada en la carencia o falta de algo, a partir de allí crear un nuevo proyecto editorial respondiendo a los interrogantes: ¿qué?, ¿para quién?, ¿cuándo?, ¿cuánto?, ¿cómo?, ¿en qué medio?, etc
+
+      .row(numero="2" titulo="Requerimientos:")
+        .col-md-4
+          figure
+            img(src='@/assets/template/tema-4-3.png', alt='Texto que describa la imagen')
+
+        .col-md-8.mb-4.mb-md-0
+          p Impuestos a partir de las necesidades identificadas, y que son establecidas por parte del cliente, contexto, política, editorial u otra que demande el proyecto en particular.
+
+      .row(numero="3" titulo="Célula:")
+        .col-md-4
+          figure
+            img(src='@/assets/template/tema-4-4.png', alt='Texto que describa la imagen')
+
+        .col-md-8.mb-4.mb-md-0
+          p Equipo de expertos interdisciplinarios para el desarrollo del proyecto en cada etapa de su diseño, ejecución y validación.
+
+      .row(numero="4" titulo="Sesiones creativas: ")
+        .col-md-4
+          figure
+            img(src='@/assets/template/tema-4-5.png', alt='Texto que describa la imagen')
+
+        .col-md-8.mb-4.mb-md-0
+          p Equipo de expertos interdisciplinarios para el desarrollo del proyecto en cada etapa de su diseño, ejecución y validación.
+      .row(numero="5" titulo="Brainstorming:")
+        .col-md-4
+          figure
+            img(src='@/assets/template/tema-4-6.png', alt='Texto que describa la imagen')
+
+        .col-md-8.mb-4.mb-md-0
+          p O lluvia o tormenta de ideas. Esta herramienta generalmente es utilizada cuando se busca llegar a consensos o tomar decisiones en grupo. El brainstorming potencia los procesos creativos, y la generación de alternativas originales que ayudan en la resolución de una situación o problema.
+      .row(numero="6" titulo="Software: ")
+        .col-md-4
+          figure
+            img(src='@/assets/template/tema-4-7.png', alt='Texto que describa la imagen')
+
+        .col-md-8.mb-4.mb-md-0
+          p Los programas y aplicaciones son de vital importancia en la creación de cualquier proyecto digital. Es necesario contar con programas de diseño, interfaces y el manejo de plataformas y formatos requeridos, para la ejecución de cada proyecto editorial.
+      .row(numero="7" titulo="Validación:")
+        .col-md-4.align-self-center
+          figure
+            img(src='@/assets/template/tema-4-8.png', alt='Texto que describa la imagen')
+
+        .col-md-8.mb-4.mb-md-0
+          p.mb-4 La validación de un proyecto consiste en la aprobación y verificación de todos los pasos requeridos para la ejecución del mismo. Por lo general esta validación está aprobada por el cliente, un director de arte o diseño.
+          p La validación tiene las siguientes características: 
+          .col-12.mt-3
+            .row.mb-4
+              .col-sm.mb-5.mb-sm-0
+                ul.lista-ul
+                  li.mb-0.d-block
+                    .row
+                      .col-1.text-align-right.p-0
+                        i.fas.fa-angle-right.color-c.text-center
+                      .col-11
+                        p.mb-2 Se mide por actividades estipuladas a cada equipo.
+                  li.mb-0.d-block
+                    .row
+                      .col-1.text-align-right.p-0
+                        i.fas.fa-angle-right.color-c.text-center
+                      .col-11
+                        p.mb-2 Valida plazos en los tiempos de ejecución de las tareas y labores estipuladas.
+                  li.mb-0.d-block
+                    .row
+                      .col-1.text-align-right.p-0
+                        i.fas.fa-angle-right.color-c.text-center
+                      .col-11
+                        p.mb-2 Determina la calidad de los componentes y las entregas ejecutadas.
+                  li.mb-0.d-block
+                    .row
+                      .col-1.text-align-right.p-0
+                        i.fas.fa-angle-right.color-c.text-center
+                      .col-11
+                        p.mb-2 Tiene en cuenta el feedbacks o retroalimentación de los clientes con la empresa y de los integrantes de los equipos de trabajo.
+
+
+</template>
+
+<script>
+import Muestras from '../components/Muestras' // borrar una vez el componente "Muestras" no se necesite
+export default {
+  name: 'Tema4',
+  components: {
+    Muestras, // borrar una vez el componente "Muestras" no se necesite
+  },
+  data: () => ({
+    // variables de vue
+  }),
+}
+</script>
+
+<style lang="sass" scoped></style>
---
+++ frontend-sn/desarrollo/ECORED-BASE-2021-master-v3.2.0/src/views/Tema5.vue
@@ -0,0 +1,134 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    .titulo-principal
+      .titulo-principal__numero
+        span 5
+      h1 Arquitectura de información, Card sorting
+    
+    figure.mb-5
+      img(src="@/assets/template/tema-5-1.png", alt="Texto que describa la imagen")
+    p.mb-4 La arquitectura de la información va muy ligada al concepto UX y se puede definir como la organización de todas las partes de algo, para que sea lo más comprensible posible
+    .row.mb-4 
+      .col-12.col-lg-8
+        p.mb-2 La arquitectura de la información busca básicamente el ordenamiento de todos los datos disponibles para que el usuario de manera lógica y ordenada realice la búsqueda de forma más rápida, fácil, ágil y dinámica.
+        p.mb-4 Un ejemplo claro de cómo funciona la arquitectura de información, es compararlo con el manejo de una biblioteca, donde cada libro pertenece a una categoría específica y se dispone en un espacio delimitado para ser ubicado de manera sencilla; los libros de historia se encuentran en una estantería delimitada, los de cocina en otra, los de política en un lugar diferente y así sucesivamente; estos tomos pueden ser ordenados de acuerdo con su importancia, su volumen o tamaño; de la misma manera pasa en la arquitectura de la información, busca que los datos presenten una estructura fácil y comprensible. 
+        p.mb-4 Este ordenamiento se puede realizar por medio de categorías de productos, contenidos, palabras clave, etc. El siguiente cuadro muestra de manera más detallada cómo realizar la organización de contenidos. 
+      .col-4.d-none.d-lg-block.align-self-center
+        figure.mb-4
+          img(src="@/assets/template/tema-5-2.svg", alt="Texto que describa la imagen")
+    .h4.mb-5 Diseño de arquitectura de información. 
+    figure.mb-4
+      img(src="@/assets/template/tema-5-3.png", alt="Texto que describa la imagen")
+    figcaption.mb-5 Referencia SENA
+    .h4.mb-4 Componentes de la arquitectura de la información
+    p.mb-4 La arquitectura de la información se puede dividir en cuatro componentes: 
+    .row
+      .col-10.offset-1.borde-gris.rounded
+        .row.py-4.px-2
+          .col-3.d-none.d-lg-block.borde-gris-der.px-5
+            figure.mb-4
+              img(src="@/assets/template/tema-5-4.svg", alt="Texto que describa la imagen")
+          .col-12.col-lg-9.px-5.align-self-center
+            .h4.mb-4 Sistemas de organización:
+            p Organización de los sistemas de navegación de manera fácil y ordenada
+    .row.mt-3
+      .col-10.offset-1.borde-gris.rounded
+        .row.py-4.px-2
+          .col-3.d-none.d-lg-block.borde-gris-der.px-5
+            figure.mb-4
+              img(src="@/assets/template/tema-5-5.svg", alt="Texto que describa la imagen")
+          .col-12.col-lg-9.px-5.align-self-center
+            .h4.mb-4 Sistemas de etiquetado:
+            p Sistema mediante el cual se etiquetan los datos de información. 
+    .row.mt-3
+      .col-10.offset-1.borde-gris.rounded
+        .row.py-4.px-2
+          .col-3.d-none.d-lg-block.borde-gris-der.px-5
+            figure.mb-4
+              img(src="@/assets/template/tema-5-6.svg", alt="Texto que describa la imagen")
+          .col-12.col-lg-9.px-5.align-self-center
+            .h4.mb-4 Sistemas de navegación: 
+            p Sistemas informáticos que hacen fácil y mejoran la accesibilidad y navegación.
+    .row.mt-3
+      .col-10.offset-1.borde-gris.rounded
+        .row.py-4.px-2
+          .col-3.d-none.d-lg-block.borde-gris-der.px-5
+            figure.mb-4
+              img(src="@/assets/template/tema-5-7.svg", alt="Texto que describa la imagen")
+          .col-12.col-lg-9.px-5.align-self-center
+            .h4.mb-4 Sistemas de búsqueda: 
+            p Sistemas que facilitan y ayudan en la localización de la información específica.
+    .row.mt-5
+      .col-12.col-lg-9
+        p.mb-4 Por su parte el Card sorting es una técnica de investigación que permite identificar cómo los usuarios creen que deberían estar organizados los contenidos de un producto digital, por ejemplo: el menú de navegación de un sitio web; aportando información relevante para tomar decisiones sobre la arquitectura de información de un producto #[strong (Torresburriel, 2017).]
+        p Se realiza mediante la participación de un grupo de usuarios objetivo, a quienes se les entrega una serie de tarjetas o Post-it, en las cuales se representan los contenidos (uno a uno) de un producto o servicio, para que sean organizadas y categorizadas de acuerdo con sus criterios y expectativas; también se espera que pueden opinar respecto a la redacción y a los elementos propuestos.
+      .col-3.d-none.d-lg-block.align-self-center
+        figure.mb-4
+          img(src="@/assets/template/tema-5-8.svg", alt="Texto que describa la imagen")
+    p.mt-3.mb-4 Es una herramienta sencilla y muy útil debido a que permite involucrar a un gran número de usuarios, permitiendo obtener información importante para tomar decisiones, mediante la estructura jerárquica de la información. La técnica sirve para aplicarse en diseños de páginas web nuevas o que estén funcionando para realizar los ajustes pertinentes luego de su análisis. Es decir, se alinean las perspectivas de los usuarios con el equipo UX. El usuario debe sentirse cómodo, entender que puede agrupar las tarjetas como le parezca conveniente y que no hay respuestas buenas ni malas.
+    figure.mb-5
+      img(src="@/assets/template/tema-5-9.png", alt="Texto que describa la imagen")
+    .h4.mb-5 Tipos de Card sorting
+    .row.mb-3 
+      .col-10.offset-1
+        .cajon.color-secundario.bg-secundario-op40.px-5.py-4.mb-4
+          .row
+            .col-lg-3.d-none.d-lg-block.align-self-center
+              figure.mb-5
+                img(src="@/assets/template/tema-5-10.svg", alt="Texto que describa la imagen")
+            .col-12.col-lg-8.align-self-center
+              .h5 Abierto: 
+              p Consiste en entregar un grupo de tarjetas a los usuarios y ellos se encargan de crear las categorías que deseen, proponiendo los nombres de estas.
+    .row.mb-3 
+      .col-10.offset-1
+        .cajon.color-secundario.bg-secundario-op25.px-5.py-4.mb-4
+          .row
+            .col-lg-3.d-none.d-lg-block.align-self-center
+              figure.mb-5
+                img(src="@/assets/template/tema-5-11.svg", alt="Texto que describa la imagen")
+            .col-12.col-lg-8.align-self-center
+              .h5 Cerrado: 
+              p En este tipo de Card Sorting, ya están creadas las diferentes categorías y los usuarios deben ordenarlas; es decir, aquí el usuario sólo puede proponer el orden en que deben organizarse los contenidos.
+    .row.mb-3 
+      .col-10.offset-1
+        .cajon.color-secundario.bg-secundario-op10.px-5.py-4.mb-4
+          .row
+            .col-lg-3.d-none.d-lg-block.align-self-center
+              figure.mb-5
+                img(src="@/assets/template/tema-5-12.svg", alt="Texto que describa la imagen")
+            .col-12.col-lg-8.align-self-center
+              .h5 Hibrido: 
+              p Es una combinación de los dos tipos anteriores. El usuario recibe las tarjetas con categorías ya determinadas, ellos las organizan, pero también tienen la posibilidad de crear otras que deseen o les parezcan convenientes.
+    p.mb-5 En la actualidad, existen programas de software que coadyuvan en esta tarea, evitando tener tarjetas físicas y el programa se encarga de agrupar los resultados para analizarlos fácilmente, como el software Optimal Sort.
+    
+    .titulo-segundo.mt-5
+      #t_5_1.h4 5.1  Evaluación heurística o de usabilidad
+    p.mb-4 Luego de la implementación de la arquitectura de información por medio del uso de tarjetas, el proceso de evaluación heurística o metodología de inspección en experiencia de usuario busca identificar y resolver aquellas problemáticas presentes en la usabilidad de los componentes de un producto, teniendo como principal objetivo medir la facilidad de uso, que se centra en el conocimiento del diseño y la experiencia de usuario. 
+    .bloque-texto-a.color-acento-contenido.p-4.p-md-5.mb-5 
+      .row.m-0.align-items-center.justify-content-between
+        .col-lg-3.d-none.d-lg-block.px-3
+          figure
+            img(src="@/assets/template/tema-5-13.svg", alt="Texto que describa la imagen").w-100
+        .col-lg-8
+          .bloque-texto-a__texto.p-4
+            .h4 El decálogo de principios heurísticos de Jakob Nielsen
+            p Este decálogo desarrollado por Jakob Nielsen, reconocido experto mundial en usabilidad de la web, da diez principios heurísticos, centrados en mejorar la experiencia de usuario, en el desarrollo de posible resolución de problemas.
+
+</template>
+
+<script>
+import Muestras from '../components/Muestras' // borrar una vez el componente "Muestras" no se necesite
+export default {
+  name: 'Tema1',
+  components: {
+    Muestras, // borrar una vez el componente "Muestras" no se necesite
+  },
+  data: () => ({
+    // variables de vue
+  }),
+}
+</script>
+
+<style lang="sass" scoped></style>
